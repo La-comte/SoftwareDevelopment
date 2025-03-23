bd0005c0be6091414620ea2ccbb7de97c19be500
Alexandra Sergeichik <8aleka.serg@gmail.com>
2025-03-23 18:43:19 +0300

    lab4 final

--- app/src/main/java/ru/iu3/myapplication/MainActivity.java
+++ app/src/main/java/ru/iu3/myapplication/MainActivity.java
@@ -9,13 +9,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.io.IOUtils;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import ru.iu3.myapplication.databinding.ActivityMainBinding;
 
@@ -62,28 +69,28 @@ public class MainActivity extends AppCompatActivity implements TransactionEvents
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        int res = initRng();
-
-        activityResultLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback() {
-                    @Override
-                    public void onActivityResult(Object o) {
-                        ActivityResult result = (ActivityResult) o;
-                        if (result.getResultCode() == Activity.RESULT_OK) {
-                            Intent data = result.getData();
-                            // обработка результата
-                            pin = data.getStringExtra("pin");
-
-                            synchronized (MainActivity.this) {
-                                MainActivity.this.notifyAll();
-                            }
-                        }
-                    }
-                }
-        );
-        byte[] trd = stringToHex("9F0206000000000100");
-        transaction(trd);
+//        int res = initRng();
+//
+//        activityResultLauncher = registerForActivityResult(
+//                new ActivityResultContracts.StartActivityForResult(),
+//                new ActivityResultCallback() {
+//                    @Override
+//                    public void onActivityResult(Object o) {
+//                        ActivityResult result = (ActivityResult) o;
+//                        if (result.getResultCode() == Activity.RESULT_OK) {
+//                            Intent data = result.getData();
+//                            // обработка результата
+//                            pin = data.getStringExtra("pin");
+//
+//                            synchronized (MainActivity.this) {
+//                                MainActivity.this.notifyAll();
+//                            }
+//                        }
+//                    }
+//                }
+//        );
+//        byte[] trd = stringToHex("9F0206000000000100");
+//        transaction(trd);
 
     }
 
@@ -101,12 +108,12 @@ public class MainActivity extends AppCompatActivity implements TransactionEvents
 
 
     public void onButtonClick(View v) {
-        Intent it = new Intent(this, PinpadActivity.class);
-        it.putExtra("amount", "100"); // пример значения
-        it.putExtra("ptc", 0);              // пример количества попыток
-//        startActivity(it);
-        activityResultLauncher.launch(it);
-
+//        Intent it = new Intent(this, PinpadActivity.class);
+//        it.putExtra("amount", "100"); // пример значения
+//        it.putExtra("ptc", 0);              // пример количества попыток
+////        startActivity(it);
+//        activityResultLauncher.launch(it);
+        testHttpClient();
     }
 
 
@@ -123,4 +130,34 @@ public class MainActivity extends AppCompatActivity implements TransactionEvents
     public static native byte[] encrypt(byte[] key, byte[] data);
 
     public static native byte[] decrypt(byte[] key, byte[] data);
+
+    protected void testHttpClient() {
+        new Thread(() -> {
+            try {
+                HttpURLConnection uc = (HttpURLConnection)
+                        (new URL("http://10.0.2.2:7777/api/v1/title").openConnection());
+                InputStream inputStream = uc.getInputStream();
+                String html = IOUtils.toString(inputStream);
+                String title = getPageTitle(html);
+                runOnUiThread(() ->
+                {
+                    Toast.makeText(this, title, Toast.LENGTH_LONG).show();
+                });
+
+            } catch (Exception ex) {
+                Log.e("fapptag", "Http client fails", ex);
+            }
+        }).start();
+    }
+
+    protected String getPageTitle(String html) {
+        Pattern pattern = Pattern.compile("<title>(.+?)</title>", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(html);
+        String p;
+        if (matcher.find())
+            p = matcher.group(1);
+        else
+            p = "Not found";
+        return p;
+    }
 }
